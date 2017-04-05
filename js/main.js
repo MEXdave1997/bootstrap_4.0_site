db3e880970a8666cb897f2a439a9707c818ad807
David Madrigal-Hernandez <david.mhernandez97@outlook.com>
2017-04-04 20:38:01 -0700

    more link fixes (maybe?)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 function main() {
-	$
+	$('#About').on('click', function() {
+		$('html,body').animate({
+			scrollTop:$('#About').offset().top
+		}, 1500);
+	});
 	$('.nav-item').on('click', function() {
 		$(this).toggleClass('active');
 	});
