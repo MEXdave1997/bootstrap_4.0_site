85e3a2a114ef019426ecb89604c2bcf8f5364828
David Madrigal-Hernandez <david.mhernandez97@outlook.com>
2017-04-05 11:16:55 -0700

    fixed styling under contact box + added navlink higlight with active class. Also fixed issue with section headers not showing.

--- js/main.js
+++ js/main.js
@@ -4,13 +4,20 @@ function main() {
 		if (target.length) {
 			event.preventDefault();
 			$('html,body').animate({
-				scrollTop: target.offset().top
+				scrollTop: target.offset().top-60
 			}, 1000);
 		}
 	});
-	// $('.nav-item').on('click', function() {
-	// 	$(this).toggleClass('active');
-	// });
+	$('.navbar-nav li a').on('click', function(e){
+		$ ('.navbar-nav li').removeClass('active');
+
+		var $parent = $(this).parent();
+		if (!$parent.hasClass('active')) {
+			$parent.addClass('active');
+		}
+
+		e.preventDefault();
+	});
 }
 
 $(document).ready(main);
