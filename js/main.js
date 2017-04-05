90d353cccf0da57c7f5f33409d4c729338c8d37c
David Madrigal-Hernandez <david.mhernandez97@outlook.com>
2017-04-04 21:12:30 -0700

    added smooth scrolling functionality to site =

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 function main() {
-	$('#About').on('click', function() {
-		$('html,body').animate({
-			scrollTop:$('#About').offset().top
-		}, 1500);
+	$('a[href^="#"]').on('click', function(event) {
+		var target = $(this.getAttribute('href'));
+		if (target.length) {
+			event.preventDefault();
+			$('html,body').animate({
+				scrollTop: target.offset().top
+			}, 1000);
+		}
 	});
-	$('.nav-item').on('click', function() {
-		$(this).toggleClass('active');
-	});
+	// $('.nav-item').on('click', function() {
+	// 	$(this).toggleClass('active');
+	// });
 }
 
 $(document).ready(main);
